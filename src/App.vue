58a72be2b7061613591385886409115d00478201
Danylo Asieiev <asieiev.danylo@spacewl.com>
2020-06-11 17:51:25 +0300

    header + content

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,170 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+
+
+
+    <div class="preloader">
+      <div class="preloader__row">
+        <div class="preloader__item"></div>
+        <div class="preloader__item"></div>
+      </div>
+    </div>
+    <div id="header">
+      <div
+       class="logo"
+       data-aos="fade-down"
+       data-aos-delay="400"
+       data-aos-duration="2000"
+       ata-aos-anchor-placement="right-left"
+     >
+     <img src="./assets/logo.svg">
+     </div>
+
+     <div class="title">
+       <div class="space">
+         <div data-aos="fade-right"
+         data-aos-delay="350"
+         data-aos-duration="2000"
+         ata-aos-anchor-placement="right-left"
+         >
+           <span>S</span>
+         </div>
+         <div data-aos="fade-right"
+         data-aos-delay="300"
+         data-aos-duration="2000"
+         ata-aos-anchor-placement="right-left"
+         >
+           <span>P</span>
+         </div>
+         <div data-aos="fade-right"
+         data-aos-delay="250"
+         data-aos-duration="2000"
+         ata-aos-anchor-placement="right-left"
+         >
+           <span>A</span>
+         </div>
+         <div data-aos="fade-right"
+         data-aos-delay="200"
+         data-aos-duration="2000"
+         ata-aos-anchor-placement="right-left"
+         >
+           <span>C</span>
+         </div>
+         <div data-aos="fade-right"
+         data-aos-delay="150"
+         data-aos-duration="2000"
+         ata-aos-anchor-placement="right-left"
+         >
+           <span>E</span>
+         </div>
+       </div>
+       <h2>TEST</h2>
+       <div class="whale">
+         <div data-aos="fade-right"
+         data-aos-delay="600"
+         data-aos-duration="2000"
+         ata-aos-anchor-placement="right-left"
+         >
+           <span>W</span>
+         </div>
+         <div data-aos="fade-right"
+         data-aos-delay="650"
+         data-aos-duration="2000"
+         ata-aos-anchor-placement="right-left"
+         >
+           <span>H</span>
+         </div>
+         <div data-aos="fade-right"
+         data-aos-delay="700"
+         data-aos-duration="2000"
+         ata-aos-anchor-placement="right-left"
+         >
+           <span>A</span>
+         </div>
+         <div data-aos="fade-right"
+         data-aos-delay="750"
+         data-aos-duration="2000"
+         ata-aos-anchor-placement="right-left"
+         >
+           <span>L</span>
+         </div>
+         <div data-aos="fade-right"
+         data-aos-delay="800"
+         data-aos-duration="1800"
+         ata-aos-anchor-placement="right-left"
+         >
+           <span>E</span>
+         </div>
+       </div>
+     </div>
+
+     <div id="triger" class="circle"
+     data-aos="fade-up"
+     data-aos-delay="1000"
+     data-aos-duration="1200">
+     </div>
+    </div>
+    <div >
+
+    </div>
+    <div class="menu"
+         data-aos="fade-down"
+         data-aos-anchor="#triger"
+         data-aos-duration="1000"
+         data-aos-anchor-placement="top-top">
+         <img src="./assets/logo.svg" alt="">
+      <ul>
+        <a href="#"><li>Home</li></a>
+        <a href="#"><li>About us</li></a>
+        <a href="#"><li>Projects</li></a>
+        <a href="#"><li>Contacts</li></a>
+      </ul>
+    </div>
+
+    <div id="content">
+      <div
+        class="content-text"
+        data-aos="fade-right"
+        data-aos-offset="100"
+        data-aos-delay="100"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out"
+        data-aos-mirror="true"
+        data-aos-once="false"
+      >
+      <p>Lorem ipsum dolor sit amet</p>
+      </div>
+      <div
+        class="content-text"
+        data-aos="flip-up"
+      >
+      <p>Lorem ipsum dolor sit amet</p>
+      </div>
+      <div
+        class="content-text"
+        data-aos="zoom-in-down"
+      >
+      <p>Lorem ipsum dolor sit amet</p>
+      </div>
+      <div
+        class="anim-text"
+        data-aos="zoom-out"
+        data-aos-offset="50"
+      >
+      <p>Lorem ipsum dolor sit amet</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout( () => {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 500);
+  }
 export default {
   name: 'app',
   data () {
@@ -31,13 +176,22 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap');
+@import './style/keyframes.scss';
+@import './style/loader.scss';
+@import './style/header.scss';
+@import './style/content.scss';
+
+body{
+  overflow-x: hidden;
+    background: #222;
+    margin: 0;
+    padding: 0;
+    font-family: 'Montserrat', 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #fff;
 }
 
 h1, h2 {
@@ -54,7 +208,4 @@ li {
   margin: 0 10px;
 }
 
-a {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 
 new Vue({
   el: '#app',
